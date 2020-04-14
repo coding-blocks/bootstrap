1abff527ab0c974f2932614bdaa374a714cbe801
Arnav Gupta <arnav@codingblocks.com>
2020-04-14 08:54:05 +0530

    update readme
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- js/dist/tooltip.js
+++ js/dist/tooltip.js
@@ -1,5 +1,5 @@
 /*!
-  * Bootstrap tooltip.js v4.4.1 (https://getbootstrap.com/)
+  * Bootstrap tooltip.js v4.4.2-beta.0 (https://getbootstrap.com/)
   * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
