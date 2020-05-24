4961620a87a7dd29ff1105c3467314376aa0e755
Arnav Gupta <arnav@codingblocks.com>
2020-05-24 11:09:34 +0530

    cb: v4.5.1 release
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- js/dist/alert.js
+++ js/dist/alert.js
@@ -1,5 +1,5 @@
 /*!
-  * Bootstrap alert.js v4.5.0 (https://getbootstrap.com/)
+  * Bootstrap alert.js v4.5.1 (https://getbootstrap.com/)
   * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
